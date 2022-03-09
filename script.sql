95dc468fa8c55d5835561f2ac9c6da3db9ef795d
Orlando0309 <79406939+Orlando0309@users.noreply.github.com>
2022-03-09 10:47:11 +0300

    Update script.sql
    io ilay script an'ireo conception vao nalefa

--- script.sql
+++ script.sql
@@ -28,3 +28,79 @@ CREATE TABLE Ecole.Classe (
 ALTER TABLE Ecole.Classe ADD CONSTRAINT Fk_Classe_Ecolage FOREIGN KEY ( idClasse ) REFERENCES Ecole.Ecolage( idClasse ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE Ecole.Etudiant ADD CONSTRAINT Fk_Etudiant_Ecolage FOREIGN KEY ( idEtudiant ) REFERENCES Ecole.Ecolage( idEtudiant ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+CREATE TABLE IF NOT EXISTS Ecole.EtudiantLogin(
+    idEtudiantLogin int NOT NULL,
+    login VARCHAR(30) NOT NULL,
+    mdp VARCHAR(30) NOT NULL,
+    idEtudiant int NOT NULL,
+    CONSTRAINT pk_EtudiantLogin_ PRIMARY KEY (idEtudiantLogin),
+    CONSTRAINT fk_EtudiantLogin_idEtudiant1 FOREIGN KEY (idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant)
+);
+
+CREATE TABLE Ecole.Paiement(
+    idPaiement int NOT NULL,
+    idEtudiant int NOT NULL,
+    montant integer CHECK(montant>0),
+    progression integer,
+    CONSTRAINT fk_Paiement_idEtudiant1 FOREIGN KEY (idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant)
+);
+
+
+CREATE TABLE Ecole.Matiere(
+    idMatiere int NOT NULL,
+    nomMatiere varchar(40) NOT NULL ,
+    Coefficient int not null  CHECK(coefficient>0),
+      CONSTRAINT pk_matiere_idmatiere PRIMARY KEY (idMatiere)
+);
+
+
+CREATE TABLE Ecole.Classe(
+    idClasse int NOT NULL ,
+    idMatiere int NOT NULL,
+    nomClasse varchar(20) NOT NULL,
+    CONSTRAINT PK_CLASSE_idclasse PRIMARY KEY(idClasse),
+    CONSTRAINT FK_Classe_idMatiere  FOREIGN KEY(idMatiere) REFERENCES Matiere(idMatiere)
+);
+
+
+CREATE TABLE Ecole.Examen(
+    idExamen int NOT NULL ,
+    periode int NOT NULL,
+    idClasse   int  NOT NULL  ,
+    anneeScolaire int NOT NULL,
+    CONSTRAINT PK_Examen_idExamen PRIMARY KEY (idExamen),
+    CONSTRAINT FK_Examen_idClasse FOREIGN KEY (idClassesClasse) REFERENCES Classes(idClasse)
+);
+
+CREATE TABLE Ecole.Bulletin(
+    idBulletin INTEGER NOT NULL,
+    idExamen INTEGER NOT NULL,
+    idEtudiant INT NOT NULL,
+    note int check (note>=0),
+    CONSTRAINT PK_Bulletin_idBulletin PRIMARY KEY (idBulletin),
+    CONSTRAINT FK_Bulletin_idExamen FOREIGN KEY (idExamen) REFERENCES Ecole.Examen (idExamen),
+    CONSTRAINT FK_Bulletin_idEtudiant FOREIGN KEY (idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant)
+);
+
+CREATE TABLE Ecole.prof(
+    idProf INTEGER NOT NULL PRIMARY KEY,
+    login VARCHAR(30),
+    mdp VARCHAR(30)
+);
+
+CREATE TABLE Ecole.admin(
+    idAdmin INTEGER NOT NULL PRIMARY KEY,
+    login VARCHAR(30),
+    mdp VARCHAR(30)
+);
+
+CREATE TABLE Ecole.fichescolaire(
+    idFicheScolaire INTEGER NOT NULL PRIMARY KEY,
+    mois integer NOT NULL CHECK (mois<=12),
+    anneeScolaire integer NOT NULL CHECK (anneeScolaire>0),
+    idEtudiant int NOT NULL,
+    idClasse int NOT NULL,
+    numero int NOT NULL,
+    CONSTRAINT FK_FicheScolaire_idEtudiant FOREIGN KEY(idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant),
+    CONSTRAINT FK_FicheScolaire_idClasse FOREIGN KEY(idClasse) REFERENCES Ecole.Classe(idClasse)
+);
