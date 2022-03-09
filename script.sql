40e0f4f443f26a67029fe70766ef7be5fbfa4852
Orlando0309 <79406939+Orlando0309@users.noreply.github.com>
2022-03-09 13:19:20 +0300

    rectification apres teste du metier
    rectification

--- script.sql
+++ script.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA Ecole;
 
-CREATE TABLE Ecole.Ecolage ( 
+
+CREATE TABLE Ecolage ( 
 	idEcolage            int  NOT NULL  ,
 	montant              int  NOT NULL  ,
 	progression          int  NOT NULL  ,
@@ -11,7 +11,7 @@ CREATE TABLE Ecole.Ecolage (
 	CONSTRAINT Unq_Ecolage_idClasse UNIQUE ( idClasse )
  );
 
-CREATE TABLE Ecole.Etudiant ( 
+CREATE TABLE Etudiant ( 
 	idEtudiant           int  NOT NULL  ,
 	nom                  varchar(60)  NOT NULL  ,
 	login                varchar(20)  NOT NULL  ,
@@ -19,34 +19,36 @@ CREATE TABLE Ecole.Etudiant (
 	CONSTRAINT Pk_Etudiant_idEtudiant PRIMARY KEY ( idEtudiant )
  );
 
-CREATE TABLE Ecole.Classe ( 
+CREATE TABLE Classe ( 
 	idClasse             int  NOT NULL  ,
 	designation          varchar(4)  NOT NULL  ,
 	CONSTRAINT Pk_Classe_idClasse PRIMARY KEY ( idClasse )
  );
 
-ALTER TABLE Ecole.Classe ADD CONSTRAINT Fk_Classe_Ecolage FOREIGN KEY ( idClasse ) REFERENCES Ecole.Ecolage( idClasse ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE Classe ADD CONSTRAINT Fk_Classe_Ecolage FOREIGN KEY ( idClasse ) REFERENCES Ecolage( idClasse ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE Ecole.Etudiant ADD CONSTRAINT Fk_Etudiant_Ecolage FOREIGN KEY ( idEtudiant ) REFERENCES Ecole.Ecolage( idEtudiant ) ON DELETE NO ACTION ON UPDATE NO ACTION;
-CREATE TABLE IF NOT EXISTS Ecole.EtudiantLogin(
+ALTER TABLE Etudiant ADD CONSTRAINT Fk_Etudiant_Ecolage FOREIGN KEY ( idEtudiant ) REFERENCES Ecolage( idEtudiant ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+CREATE TABLE IF NOT EXISTS EtudiantLogin(
     idEtudiantLogin int NOT NULL,
     login VARCHAR(30) NOT NULL,
     mdp VARCHAR(30) NOT NULL,
     idEtudiant int NOT NULL,
     CONSTRAINT pk_EtudiantLogin_ PRIMARY KEY (idEtudiantLogin),
-    CONSTRAINT fk_EtudiantLogin_idEtudiant1 FOREIGN KEY (idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant)
+    CONSTRAINT fk_EtudiantLogin_idEtudiant1 FOREIGN KEY (idEtudiant) REFERENCES Etudiant(idEtudiant)
 );
 
-CREATE TABLE Ecole.Paiement(
+CREATE TABLE Paiement(
     idPaiement int NOT NULL,
     idEtudiant int NOT NULL,
     montant integer CHECK(montant>0),
     progression integer,
-    CONSTRAINT fk_Paiement_idEtudiant1 FOREIGN KEY (idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant)
+    CONSTRAINT fk_Paiement_idEtudiant1 FOREIGN KEY (idEtudiant) REFERENCES Etudiant(idEtudiant)
 );
 
 
-CREATE TABLE Ecole.Matiere(
+CREATE TABLE Matiere(
     idMatiere int NOT NULL,
     nomMatiere varchar(40) NOT NULL ,
     Coefficient int not null  CHECK(coefficient>0),
@@ -54,7 +56,7 @@ CREATE TABLE Ecole.Matiere(
 );
 
 
-CREATE TABLE Ecole.Classe(
+CREATE TABLE Classe(
     idClasse int NOT NULL ,
     idMatiere int NOT NULL,
     nomClasse varchar(20) NOT NULL,
@@ -63,88 +65,88 @@ CREATE TABLE Ecole.Classe(
 );
 
 
-CREATE TABLE Ecole.Examen(
+CREATE TABLE Examen(
     idExamen int NOT NULL ,
     periode int NOT NULL,
     idClasse   int  NOT NULL  ,
     anneeScolaire int NOT NULL,
     CONSTRAINT PK_Examen_idExamen PRIMARY KEY (idExamen),
-    CONSTRAINT FK_Examen_idClasse FOREIGN KEY (idClassesClasse) REFERENCES Classes(idClasse)
+    CONSTRAINT FK_Examen_idClasse FOREIGN KEY (idClasse) REFERENCES Classe(idClasse)
 );
 
-CREATE TABLE Ecole.Bulletin(
+CREATE TABLE Bulletin(
     idBulletin INTEGER NOT NULL,
     idExamen INTEGER NOT NULL,
     idEtudiant INT NOT NULL,
     note int check (note>=0),
     CONSTRAINT PK_Bulletin_idBulletin PRIMARY KEY (idBulletin),
-    CONSTRAINT FK_Bulletin_idExamen FOREIGN KEY (idExamen) REFERENCES Ecole.Examen (idExamen),
-    CONSTRAINT FK_Bulletin_idEtudiant FOREIGN KEY (idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant)
+    CONSTRAINT FK_Bulletin_idExamen FOREIGN KEY (idExamen) REFERENCES Examen (idExamen),
+    CONSTRAINT FK_Bulletin_idEtudiant FOREIGN KEY (idEtudiant) REFERENCES Etudiant(idEtudiant)
 );
 
-CREATE TABLE Ecole.prof(
+CREATE TABLE prof(
     idProf INTEGER NOT NULL PRIMARY KEY,
     login VARCHAR(30),
     mdp VARCHAR(30)
 );
 
-CREATE TABLE Ecole.admin(
+CREATE TABLE admin(
     idAdmin INTEGER NOT NULL PRIMARY KEY,
     login VARCHAR(30),
     mdp VARCHAR(30)
 );
 
-CREATE TABLE Ecole.fichescolaire(
+CREATE TABLE fichescolaire(
     idFicheScolaire INTEGER NOT NULL PRIMARY KEY,
     mois integer NOT NULL CHECK (mois<=12),
     anneeScolaire integer NOT NULL CHECK (anneeScolaire>0),
     idEtudiant int NOT NULL,
     idClasse int NOT NULL,
     numero int NOT NULL,
-    CONSTRAINT FK_FicheScolaire_idEtudiant FOREIGN KEY(idEtudiant) REFERENCES Ecole.Etudiant(idEtudiant),
-    CONSTRAINT FK_FicheScolaire_idClasse FOREIGN KEY(idClasse) REFERENCES Ecole.Classe(idClasse)
+    CONSTRAINT FK_FicheScolaire_idEtudiant FOREIGN KEY(idEtudiant) REFERENCES Etudiant(idEtudiant),
+    CONSTRAINT FK_FicheScolaire_idClasse FOREIGN KEY(idClasse) REFERENCES Classe(idClasse)
 );
 
-CREATE SEQUENCE Ecole.classe_seq
+CREATE SEQUENCE classe_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.examen_seq
+CREATE SEQUENCE examen_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.bulletin_seq
+CREATE SEQUENCE bulletin_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.etudiantlogin_seq
+CREATE SEQUENCE etudiantlogin_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.paiement_seq
+CREATE SEQUENCE paiement_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.matiere_seq
+CREATE SEQUENCE matiere_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.admin_seq
+CREATE SEQUENCE admin_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
 
-CREATE SEQUENCE Ecole.prof_seq
+CREATE SEQUENCE prof_seq
     INCREMENT 1
     START 1
     MINVALUE 1;
-    
+
 CREATE SEQUENCE public.fichierscolaire_seq
     INCREMENT 1
     START 1
